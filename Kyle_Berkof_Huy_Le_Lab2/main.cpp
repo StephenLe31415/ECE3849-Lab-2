403dd43d13b095e36e94ac9b137f53bc828560cd
Huy Le <62473020+StephenLe31415@users.noreply.github.com>
2025-11-05 10:35:26 -0500

    main.cpp with button implementation

--- Kyle_Berkof_Huy_Le_Lab2/main.cpp
+++ Kyle_Berkof_Huy_Le_Lab2/main.cpp
@@ -1,66 +1,58 @@
 #include <stdint.h>
 #include <stdbool.h>
-#include <stdio.h>
 
-extern "C" {
-#include "driverlib/fpu.h"
-#include "driverlib/interrupt.h"
+//extern "C" {
 #include "driverlib/sysctl.h"
 #include "driverlib/gpio.h"
 #include "driverlib/pin_map.h"
 #include "inc/hw_memmap.h"
 #include "FreeRTOS.h"
 #include "task.h"
+//}
 
-#include "display/HAL_EK_TM4C1294XL_Crystalfontz128x128_ST7735.h"
-#include "display/Crystalfontz128x128_ST7735.h"
+#include "button.h"
+static constexpr uint32_t BUTTON_TICK_MS     = 20U;
+
+// LED1: PN0 (D1 Green) - LED2: PN1 (D2 Blue)
+#define LED1_PORT GPIO_PORTN_BASE
+#define LED1_PIN  GPIO_PIN_0
+
+#define LED2_PORT GPIO_PORTN_BASE
+#define LED2_PIN  GPIO_PIN_1
+
+/****** BUTTON ******/
+void OnPlayPauseClick() {
+    // Do something with the buzzer
+    // gRunning = !gRunning;
 
 }
 
-
-#define DISPLAY_REFRESH_MS 16
-#define TIMER_REFRESH_MS 1
-
-
-tContext gContext;
-
-volatile uint8_t hours = 0;
-volatile uint8_t minutes = 0;
-volatile uint8_t seconds = 0;
-volatile uint16_t milliseconds = 0;
-
-volatile uint32_t gStopwatchMs = 0;
-
-void displayTask(void* args) {
-    while(1) {
-
-        tRectangle background = {0, 0, 127, 127};
-        GrContextForegroundSet(&gContext, ClrBlack);
-        GrRectFill(&gContext, &background);
-
-        char str[20];
-        snprintf(str, sizeof(str), "%02u:%02u:%02u:%03u", hours, minutes, seconds, milliseconds);
-
-        GrContextForegroundSet(&gContext, ClrYellow);
-        GrStringDrawCentered(&gContext, str, -1, 64, 50, false);
-
-        GrFlush(&gContext);
-
-        vTaskDelay(pdMS_TO_TICKS(TIMER_REFRESH_MS));
-    }
+void OnResetClick() {
+    // Do something with the buzzer
+    // global_timer = 0;
 }
 
-void timerTask(void* args) {
+void buttonTask(void *pvParameters) {
+    // Button Init
+    Button btnPlayPause(S1);
+    Button btnReset(S2);
+    
+    btnPlayPause.begin(); 
+    btnPlayPause.setDebounceMs(30U);
+//    btnPlayPause.setTickInterval(20U);
+    
+    btnReset.begin();
+    btnReset.setDebounceMs(30U);
+//    btnReset.setTickInterval(20U);
+
+    btnPlayPause.attachClick(&OnPlayPauseClick);
+    btnReset.attachClick(&OnResetClick);
+
     while(1) {
-        seconds = (gStopwatchMs / 1000U) % 60U;
-        minutes = (gStopwatchMs / 60000U) % 60U;
-        hours = gStopwatchMs / 3600000U;
-        milliseconds = gStopwatchMs % 1000U;
-
-        gStopwatchMs ++;
-
-        vTaskDelay(pdMS_TO_TICKS(TIMER_REFRESH_MS));
-    }
+        btnPlayPause.tick();
+        btnReset.tick();
+        vTaskDelay(pdMS_TO_TICKS(BUTTON_TICK_MS));              // Wait 16ms
+    }    
 }
 
 int main(void) {
@@ -76,18 +68,23 @@ int main(void) {
         120000000
     );
 
+    // Enable GPIO peripherals
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPION);
+    while (!SysCtlPeripheralReady(SYSCTL_PERIPH_GPION));
+
     //Initialize LCD
     Crystalfontz128x128_Init();
     Crystalfontz128x128_SetOrientation(LCD_ORIENTATION_UP);
     GrContextInit(&gContext, &g_sCrystalfontz128x128);
     GrContextFontSet(&gContext, &g_sFontFixed6x8);
 
+    //Initialize
+
     /* Now enable interrupts globally (FreeRTOS will manage nesting later) */
     IntMasterEnable();
 
     // Create tasks
-    xTaskCreate(displayTask, "Display", 512, NULL, tskIDLE_PRIORITY + 2, NULL);
-    xTaskCreate(timerTask, "Timer", 512, NULL, tskIDLE_PRIORITY + 3, NULL);
+    xTaskCreate(buttonTask, "Button", 512, NULL, tskIDLE_PRIORITY + 1, NULL);
 
     // Start scheduler
     vTaskStartScheduler();
@@ -95,6 +92,3 @@ int main(void) {
     // Should never reach here
     while (1);
 }
-
-
-//NOTE: PROBLEM during lab: stack size was too small on xTaskCreate
